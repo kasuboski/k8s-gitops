d3c623b2050ed38163977fad13bb89e58aff7009
Josh Kasuboski <josh.kasuboski@gmail.com>
2021-10-31 17:00:49 -0500

    Run transmission with doppler secret

--- hack/create-doppler-tokens.sh
+++ hack/create-doppler-tokens.sh
@@ -2,9 +2,10 @@
 
 set -ex
 
-APPS="grafana-agent"
+APPS="grafana-agent media"
 
 for a in $APPS
 do
+    kubectl delete secret "$a-token" -n doppler-operator-system --ignore-not-found
     kubectl create secret generic "$a-token" -n doppler-operator-system --from-literal=serviceToken=$(doppler configs tokens create --project "$a" --config dev "$a-token" --plain)
 done
