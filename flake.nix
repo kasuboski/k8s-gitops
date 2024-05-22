d62405873e269eca38c4e56dd84f12de3ffee3ad
Josh Kasuboski <josh.kasuboski@gmail.com>
2024-05-21 19:58:00 -0500

    git-crypt for nix

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
       formatter = pkgs.alejandra;
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
+          git-crypt
           doppler
           kubecolor
           kubectl
