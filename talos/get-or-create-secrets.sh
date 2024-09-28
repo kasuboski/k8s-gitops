bb2ec172c74b34442a1bb4412a14194c455a86d3
Josh Kasuboski <josh.kasuboski@gmail.com>
2024-09-28 13:37:00 -0500

    talos generation maybe

---
+++ talos/get-or-create-secrets.sh
@@ -0,0 +1,14 @@
+SECRET_FILE=$1
+if [ -z "$SECRET_FILE" ]; then
+  echo "Must provide a secret file"
+  exit 1
+fi
+
+EXISTING=$(doppler secrets get SECRET_YAML -p talos -c boot --plain 2> /dev/null)
+if [ -z "$EXISTING" ]; then
+  talosctl gen secrets -o "$SECRET_FILE"
+  cat "$SECRET_FILE" | doppler secrets set SECRET_YAML -p talos -c boot
+else
+  echo "$EXISTING" > "$SECRET_FILE"
+fi
+
